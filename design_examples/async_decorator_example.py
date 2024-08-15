a06a2c41c3593078e659b2b3856042555f81f6ed
leonardoleano <leodrl9@gmail.com>
2024-08-15 16:21:17 -0300

    safe commit

--- design_examples/async_decorator_example.py
+++ design_examples/async_decorator_example.py
@@ -4,16 +4,19 @@ from functools import wraps, partial
 
 
 def to_async(func):
-    @wraps(func)  # Makes sure that function is returned for e.g. func.__name__ etc.
+    # Makes sure that function is returned for e.g. func.__name__ etc.
+    @wraps(func)
     async def run(*args, loop=None, executor=None, **kwargs):
         if loop is None:
-            loop = asyncio.get_event_loop() # Make event loop of nothing exists
-        pfunc = partial(func, *args, **kwargs)  # Return function with variables (event) filled in
+            loop = asyncio.get_event_loop()  # Make event loop of nothing exists
+        # Return function with variables (event) filled in
+        pfunc = partial(func, *args, **kwargs)
         return await loop.run_in_executor(executor, pfunc)
     return run
 
+
 @to_async
-def to_gather(sleep_time:int)->int:
+def to_gather(sleep_time: int) -> int:
     print(f"start:{sleep_time}")
     time.sleep(sleep_time)
     print(f"finish:{sleep_time}")
@@ -21,7 +24,7 @@ def to_gather(sleep_time:int)->int:
 
 
 async def main():
-    res = await asyncio.gather(*(to_gather(i) for i in [2,5,3,4,5,2,2]))
+    res = await asyncio.gather(*(to_gather(i) for i in [2, 5, 3, 4, 5, 2, 2]))
     return res
 
 if __name__ == "__main__":
